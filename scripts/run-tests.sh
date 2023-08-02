721dd342518f2b54cdef6c75938a3259642d8b9d
daniel khen <danielkhen20@gmail.com>
2023-08-02 14:55:13 +0300

    Auto-Commit: ipam variables change

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+modules_to_exclude=("ip_address_management")
 cd ..
 
 for module in modules/*/;
 do
   (
   module_name=$(basename "$module")
+
+  if [[ " ${modules_to_exclude[*]} " == *" $module_name "* ]]; then
+    echo "Skipping tests for module: $module_name"
+    continue
+  fi
+
   error_output=$(terraform -chdir="$module/tests" init -upgrade 2>&1 >/dev/null)
+
   if [ $? -ne 0 ]; then
     echo "Error in tests init $module_name:"
     echo "$error_output"
   else
     apply_output=$(terraform -chdir="$module/tests" apply -auto-approve 2>&1 >/dev/null)
+
     if [ $? -ne 0 ]; then
       echo "Error in tests apply $module_name:"
       echo "$apply_output"
     fi
 
     destroy_output=$(terraform -chdir="$module/tests" destroy -auto-approve 2>&1 >/dev/null)
+
     if [ $? -ne 0 ]; then
       echo "Error in tests destroy $module_name:"
       echo "$destroy_output"
